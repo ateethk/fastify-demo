429022d0b0a65874d3e214a54a0bccac8776c4f1
Ateeth Kosuri <ateethkosuri@gmail.com>
2019-03-02 09:57:46 -0700

    init

---
+++ server.js
@@ -0,0 +1,15 @@
+// IMPORTS
+const fastify = require('fastify')();
+
+// ROUTES
+fastify.register(require('./routes/users'), { prefix: '/users' });
+
+// LISTENER
+fastify.listen(3000, function (err, address) {
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    } else {
+        console.log('Server is up and running on port 3000...');
+    }
+}); 
